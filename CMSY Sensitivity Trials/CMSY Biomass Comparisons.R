c37bccf188a36eb78440bb474db00e76e7b01992
Paul Bouch <pbouch@Marine.ie>
2018-06-20 10:08:46 +0100

    Code to compare the Cod data when forcing various initial and final biomass priors

---
+++ CMSY Sensitivity Trials/CMSY Biomass Comparisons.R
@@ -0,0 +1,27 @@
+#### Plots to compare results
+library (ggplot2)
+library (gridExtra)
+options(scipen = 999)
+
+results_biomass_CMSY$Method = "CMSY"
+results_biomass_BSM$Method = "BSM"
+
+Biomass_test = rbind(results_biomass_CMSY, results_biomass_BSM)
+
+fmsy.plot = ggplot (Biomass_test, aes(Method, fmsy, colour = Profile))+
+  geom_point(pch=1, size = 3, position = position_dodge(width = 0.5)) +
+  geom_linerange(aes(ymin = fmsy.low, ymax = fmsy.high), position = position_dodge(width = 0.5), size =0.3)+
+  theme_bw()
+
+msy.plot = ggplot (Biomass_test, aes(Method, bmsy, colour = Profile))+
+  geom_point(pch=1, size = 3, position = position_dodge(width = 0.5)) +
+  geom_linerange(aes(ymin = bmsy.low, ymax = bmsy.high), position = position_dodge(width = 0.5), size =0.3)+
+  theme_bw()
+
+bmsy.plot = ggplot (Biomass_test, aes(Method, msy, colour = Profile))+
+  geom_point(pch=1, size = 3, position = position_dodge(width = 0.5)) +
+  geom_linerange(aes(ymin = msy.low, ymax = msy.high), position = position_dodge(width = 0.5), size =0.3)+
+  theme_bw()
+
+
+grid.arrange(fmsy.plot, msy.plot, bmsy.plot, ncol= 1)
