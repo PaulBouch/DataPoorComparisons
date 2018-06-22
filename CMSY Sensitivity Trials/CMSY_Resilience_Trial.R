1077d709f0d656809ad06c7fcbf0d5a0614843cf
Paul Bouch <pbouch@Marine.ie>
2018-06-22 11:18:09 +0100

    minor fix

--- CMSY Sensitivity Trials/CMSY_Resilience_Trial.R
+++ CMSY Sensitivity Trials/CMSY_Resilience_Trial.R
@@ -53,7 +53,7 @@ resilience = "0.1-1"
 output_wide <- cmsy2(year=cod.data$year, catch=cod.data$catch, r.low = 0.1, r.hi = 1)
 plot_dlm(output_wide)
 ref_wide <- output_wide[["ref_pts"]]
-results_resilience_BSM = write.results(results_resilience_BSM, ref_wide)
+results_resilience = write.results(results_resilience, ref_wide)
 
 results_resilience = setNames(results_resilience, c("Resilience", "r", "r.low", "r.high",
                                                "k", "k.low", "k.high", 
